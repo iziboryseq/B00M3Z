b4a8a2428a886daca5d115100452d9cd1540f79c
iziboryseq <iziborysek@gmail.com>
2026-05-27 13:16:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const mineflayer = require("mineflayer");
+
+const LOGIN_COMMAND = process.env.COMMAND;
+
+let bot;
+
+function createBot() {
+  bot = mineflayer.createBot({
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.USERNAME,
+    version: process.env.VERSION,
+    chat: process.env.CHAT,
+    viewDistance: process.env.VIEWDISTANCE,
+    physicsEnabled: false,
+    hideErrors: false,
+  });
+
+  bot.once("spawn", async () => {
+    setTimeout(() => {
+      bot.chat(LOGIN_COMMAND); //logging
+    }, 4000);
+
+    setTimeout(async () => {
+      try {
+        await bot.activateItem(); // opening mode menu
+      } catch (err) {
+        console.error(err);
+      }
+    }, 6000);
+  });
+
+  bot.once("windowOpen", async (window) => {
+    setTimeout(async () => {
+      try {
+        await bot.clickWindow(19, 0, 0); // selecting mode
+      } catch (err) {
+        console.error(err);
+      }
+    }, 3000);
+  });
+
+  bot.on("spawn", () => {
+    console.log("Spawned");
+  });
+
+  bot.on("end", () => {
+    console.log("Disconnected. Reconnecting...");
+    setTimeout(() => process.exit(1), 30_000);
+  });
+}
+
+createBot();
